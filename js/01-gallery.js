150cce5f513474f3faa8ae66873ef31e32485c32
AleksSylka <119104646+AleksSylka@users.noreply.github.com>
2023-03-31 09:58:00 +0300

    HW-7

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(galleryItems)
 
-console.log(galleryItems);
+const ulGalleryEl = document.querySelector('ul.gallery');
+let arrayItemLi = [];
+
+arrayItemLi = galleryItems.map(({ description, original, preview }) =>
+    `<li class="gallery__item">
+    <a class="gallery__link" href=${original}>
+    <img src=${preview} alt='${description}' data-source=${original} class="gallery__image">
+    </a>
+    </li>`
+).join("");
+
+ulGalleryEl.insertAdjacentHTML('beforeend', arrayItemLi);
+
+ulGalleryEl.addEventListener('click', onClick);
+function onClick(event) {
+    event.preventDefault();
+    const { target } = event;
+    if (target.nodeName === 'UL') {
+        console.log('Mimo');
+        return
+    };
+    const instance = basicLightbox.create(`
+    <img src=${target.dataset.source} alt='${target.alt}'>
+    `);
+    instance.show();
+
+    function onEscClose(elm) {
+        console.log(elm.code)
+        if (elm.code === 'Escape') {
+            instance.close();
+        }
+    }
+    document.addEventListener("keyup", onEscClose, { once: instance.visible() });
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,25 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const ulGalleryEl = document.querySelector('ul.gallery');
+let arrayItemLi = [];
+
+arrayItemLi = galleryItems.map(({ description, original, preview }) =>
+    `<li class="gallery__item">
+    <a class="gallery__link" href=${original} title="${description}">
+    <img src=${preview} alt='${description}' class="gallery__image">
+    </a>
+    </li>`
+).join("");
+
+ulGalleryEl.insertAdjacentHTML('beforeend', arrayItemLi);
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captionType: 'attr',
+    captionSelector: 'img',
+    captionsData: 'alt',
+    captions: true,
+    captionDelay: 250,
+});
+console.dir(lightbox)
